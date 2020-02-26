814861a0738b5f3c75182862d9d5dadc7bde79fe
ipritamdatta <pritamewu@gmail.com>
2020-02-26 14:15:11 +0600

    softDelete restore,permanent delete, role created and added to user

--- resources/views/blogs/trash.blade.php
+++ resources/views/blogs/trash.blade.php
@@ -2,9 +2,41 @@
 
 @section('content')
 
-@foreach( $trashedBlogs as $blog )
-    <h2>{{$blog->title}}</h2>
-    <p>{{$blog->body}}</p>
-@endforeach
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h2>Trashed Blogs</h2>
+    </div>
+</div>
+
+<div class="col-md-12">
+    @foreach( $trashedBlogs as $blog )
+        <h2>{{$blog->title}}</h2>
+        <p>{{$blog->body}}</p>
+   
+
+        {{-- restore --}}
+        <div class="btn-group">
+            <form method="get" action="{{ route('blogs.restore', $blog->id) }}" >
+                <button type="submit" class="btn btn-success btn-xs pull-left btn-margin-right">
+                    Restore
+                </button>
+                {{ csrf_field() }}
+            </form>
+
+            {{-- Permanent Delete --}}
+            <form method="post" action="{{ route('blogs.permanent-delete', $blog->id) }}" >
+                
+                {{ method_field('delete') }}
+
+                <button type="submit" class="btn btn-danger btn-xs pull-left btn-margin-right">
+                    Permanent Delete
+                </button>
+                
+                {{ csrf_field() }}
+
+            </form>
+        </div>
+     @endforeach
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ Route::get('/blogs/create','BlogsController@create')->name('blogs.create');
 Route::post('/blogs/store','BlogsController@store')->name('blogs.store');
 //  keep trashed routes here
 Route::get('/blogs/trash','BlogsController@trash')->name('blogs.trash');
-
+Route::get('/blogs/trash/{id}/restore','BlogsController@restore')->name('blogs.restore');
+Route::delete('/blogs/trash/{id}/permanent-delete','BlogsController@permanentDelete')->name('blogs.permanent-delete');
 
 Route::get('/blogs/{id}','BlogsController@show')->name('blogs.show');
 Route::get('/blogs/{id}/edit','BlogsController@edit')->name('blogs.edit');
